a1ebffd7159a5ad288764a25aa8ec796e968525d
Dicky Saputra <saputradicky705@gmail.com>
2024-05-30 18:44:15 +0700

    setup typography & button

---
+++ src/constants/themes/ThemeButton.ts
@@ -0,0 +1,27 @@
+export type TButton = typeof ThemeButton;
+
+const baseStyles = "p";
+const variantPrimaryStyles = "p";
+const variantSecondaryStyles = "p";
+const variantTertiaryStyles = "p";
+const variantDisabledStyles = "p";
+const sizeSmStyles = "p-0";
+const sizeMdStyles = "p-0";
+const sizeLgStyles = "p-0";
+const sizeXlStyles = "p-0";
+
+export const ThemeButton = {
+  base: baseStyles,
+  variant: {
+    primary: variantPrimaryStyles,
+    secondary: variantSecondaryStyles,
+    tertiary: variantTertiaryStyles,
+    disabled: variantDisabledStyles,
+  },
+  size: {
+    sm: sizeSmStyles,
+    md: sizeMdStyles,
+    lg: sizeLgStyles,
+    xl: sizeXlStyles,
+  },
+};
---
+++ src/constants/themes/ThemeTypography.ts
@@ -0,0 +1,38 @@
+export type TTypography = typeof ThemeTypography;
+
+const baseStyles = "p";
+
+const variantH1Styles = "p-0";
+const variantH2Styles = "p-0";
+const variantH3Styles = "p-0";
+const variantH4Styles = "p-0";
+const variantH5Styles = "p-0";
+const variantH6Styles = "p-0";
+const variantCaption1Styles = "p-0";
+const variantCaption2Styles = "p-0";
+const variantButton1Styles = "p-0";
+const variantButton2Styles = "p-0";
+const variantBodySmStyles = "p-0";
+const variantBodyMdStyles = "p-0";
+const variantBodyLgStyles = "p-0";
+const variantBodyXlStyles = "p-0";
+
+export const ThemeTypography = {
+  base: baseStyles,
+  variant: {
+    h1: variantH1Styles,
+    h2: variantH2Styles,
+    h3: variantH3Styles,
+    h4: variantH4Styles,
+    h5: variantH5Styles,
+    h6: variantH6Styles,
+    caption1: variantCaption1Styles,
+    caption2: variantCaption2Styles,
+    button1: variantButton1Styles,
+    button2: variantButton2Styles,
+    bodySm: variantBodySmStyles,
+    bodyMd: variantBodyMdStyles,
+    bodyLg: variantBodyLgStyles,
+    bodyXl: variantBodyXlStyles,
+  },
+};
